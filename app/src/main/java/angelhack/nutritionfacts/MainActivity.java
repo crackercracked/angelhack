e511402310f7b2d6ece99af7910412fa57addca8
crackercracked <crackercracked@hotmail.com>
2015-06-07 01:50:03 -0400

    merged camera and analysis request

--- app/src/main/java/angelhack/nutritionfacts/MainActivity.java
+++ app/src/main/java/angelhack/nutritionfacts/MainActivity.java
@@ -1,47 +1,201 @@
 package angelhack.nutritionfacts;
 
+
 import android.content.Intent;
-import android.os.Environment;
-import android.support.v7.app.ActionBarActivity;
+import android.content.pm.ActivityInfo;
+import android.database.Cursor;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements SurfaceHolder.Callback {
+
+    TextView testView;
+
+    Camera camera;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    Camera.PictureCallback rawCallback;
+    Camera.ShutterCallback shutterCallback;
+    Camera.PictureCallback jpegCallback;
+    private final String tag = "main";
+
+    Button start, stop, capture, analysis;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        // When Next is clicked send it to the secondActivity
-        Button Next = (Button)findViewById(R.id.btnNext);
-        Next.setOnClickListener(new Button.OnClickListener(){
-            public void onClick(View v) {
-                showNextIntent();
+        start = (Button)findViewById(R.id.btn_start);
+        start.setOnClickListener(new Button.OnClickListener()
+        {
+            public void onClick(View arg0) {
+                start_camera();
             }
         });
-    } // end onCreate()
+        stop = (Button)findViewById(R.id.btn_stop);
+        capture = (Button) findViewById(R.id.capture);
+        analysis = (Button) findViewById(R.id.analysis);
+        stop.setOnClickListener(new Button.OnClickListener()
+        {
+            public void onClick(View arg0) {
+                stop_camera();
+            }
+        });
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                captureImage();
+            }
+        });
+        analysis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                analysis();
+            }
+        });
 
-    private void showNextIntent() {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+        surfaceView = (SurfaceView)findViewById(R.id.surfaceView1);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        rawCallback = new Camera.PictureCallback() {
+            public void onPictureTaken(byte[] data, Camera camera) {
+                Log.d("Log", "onPictureTaken - raw");
+            }
+        };
+
+        /** Handles data for jpeg picture */
+        shutterCallback = new Camera.ShutterCallback() {
+            public void onShutter() {
+                Log.i("Log", "onShutter'd");
+            }
+        };
+        jpegCallback = new Camera.PictureCallback() {
+            public void onPictureTaken(byte[] data, Camera camera) {
+                FileOutputStream outStream = null;
+                try {
+                  //  outStream = new FileOutputStream(String.format(
+                   //         "/sdcard/%d.jpg", System.currentTimeMillis()));
+                    outStream = new FileOutputStream("/sdcard/recent.jpg");
+                    outStream.write(data);
+                    outStream.close();
+                    Log.d("Log", "onPictureTaken - wrote bytes: " + data.length);
+                    Toast.makeText(MainActivity.this, "Captured.", Toast.LENGTH_SHORT).show();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "Capture Failed.", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "Capture Failed.", Toast.LENGTH_SHORT).show();
+                } finally {
+                }
+            }
+        };
     }
+
+    private void captureImage() {
+        // TODO Auto-generated method stub
+        camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+    }
+
+    private void start_camera()
+    {
+        try{
+            camera = Camera.open();
+        }catch(RuntimeException e){
+            Log.e(tag, "init_camera: " + e);
+            return;
+        }
+        Camera.Parameters param;
+        param = camera.getParameters();
+        //modify parameter
+        param.setPreviewFrameRate(20);
+        param.setPreviewSize(176, 144);
+        camera.setParameters(param);
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+            //camera.takePicture(shutter, raw, jpeg)
+        } catch (Exception e) {
+            Log.e(tag, "init_camera: " + e);
+            return;
+        }
+    }
+
+    private void stop_camera()
+    {
+        camera.stopPreview();
+        camera.release();
+    }
+
+    private void analysis()
+    {
+
+        String path = getLastImagePath();
+        Intent intent = new Intent(this, SecondActivity.class).putExtra(Intent.EXTRA_TEXT, path);
+        startActivityForResult(intent, 0);
+        stop_camera();
+
+    }
+
+    protected File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new java.util.Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+       // CameraActivity activity = (CameraActivity)getActivity();
+        //activity.setCurrentPhotoPath("file:" + image.getAbsolutePath());
+        return image;
+    }
+
+
+
+
+    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+        // TODO Auto-generated method stub
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -64,4 +218,19 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private String getLastImagePath(){
+        final String[] imageColumns = { MediaStore.Images.Media._ID, MediaStore.Images.Media.DATA };
+        final String imageOrderBy = MediaStore.Images.Media._ID+" DESC";
+        Cursor imageCursor = managedQuery(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, imageColumns, null, null, imageOrderBy);
+        if(imageCursor.moveToFirst()){
+            String fullPath = imageCursor.getString(imageCursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            imageCursor.close();
+            return fullPath;
+        }else{
+            return "";
+        }
+    }
+
 }
+
--- app/src/main/java/angelhack/nutritionfacts/SecondActivity.java
+++ app/src/main/java/angelhack/nutritionfacts/SecondActivity.java
@@ -3,40 +3,33 @@ package angelhack.nutritionfacts;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.os.Environment;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 public class SecondActivity extends ActionBarActivity {
 
 
-    String URL = "https://api.idolondemand.com/1/api/sync/ocrdocument/v1";
+ //   String URL = "https://api.idolondemand.com/1/api/sync/ocrdocument/v1";
+    String URL = "";
+    final String apiUrl = "https://api.idolondemand.com/1/api/sync/ocrdocument/v1";
     String result = "";
     final String tag = "Your Logcat tag: ";
 
@@ -46,13 +39,17 @@ public class SecondActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        final Button btnSearch = (Button)findViewById(R.id.btnSearch);
-        btnSearch.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                String query = "asdfasdfkj";
-                callWebService(query);
-            }
-        });
+        URL = this.getIntent().getStringExtra(Intent.EXTRA_TEXT);
+        Log.e(tag, URL);
+        callWebService("");
+
+//        final Button btnSearch = (Button)findViewById(R.id.btnSearch);
+//        btnSearch.setOnClickListener(new Button.OnClickListener() {
+//            public void onClick(View v) {
+//                String query = "";
+//                callWebService(query);
+//            }
+//        });
     }
 
     @Override
@@ -85,7 +82,7 @@ public class SecondActivity extends ActionBarActivity {
     }
 
 
-    private class WebService extends AsyncTask<String, Void, String>{
+    private class WebService extends AsyncTask<String, Void, String> {
 
         ProgressDialog dialog = new ProgressDialog(SecondActivity.this);
 
@@ -99,9 +96,13 @@ public class SecondActivity extends ActionBarActivity {
         protected String doInBackground(String... params) {
 
             HttpClient httpclient = new DefaultHttpClient();
-            HttpPost request = new HttpPost(URL);
+            HttpPost request = new HttpPost(apiUrl);
             MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create();
-            File file = new File(Environment.getExternalStorageDirectory(), "b.jpg");
+            File file = new File(URL);
+
+          //  File file = new File(Environment.getExternalStorageDirectory(), "b.jpg");
+         //   File file = new File(Environment.getExternalStorageDirectory(), "/sdcard/recent.jpg");
+
             entityBuilder.addBinaryBody("file", file, ContentType.MULTIPART_FORM_DATA, file.getName());
             entityBuilder.addTextBody("apikey", "dd5e679c-3e9b-4ee6-ab4c-9db34501fb66");
             entityBuilder.addTextBody("mode", "document_scan");
@@ -115,21 +116,19 @@ public class SecondActivity extends ActionBarActivity {
             } catch (IOException e) {
                 e.printStackTrace();
                 return "Fail";
-            } catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
-                return"Fail";
+                return "Fail";
             }
-            return result;
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String result){
+                Toast.makeText(SecondActivity.this, result, Toast.LENGTH_LONG).show();
+                dialog.dismiss();
+            }
         }
-
-        @Override
-        protected void onPostExecute(String result){
-            Toast.makeText(SecondActivity.this, result, Toast.LENGTH_LONG).show();
-            dialog.dismiss();
-        }
-    }
-
-
 
 
 
